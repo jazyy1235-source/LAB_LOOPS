d98eace72f34a6ac95b3f65fa993a64508d944dc
Aljazy <jazyy1235@gmail.com>
2026-09-09 18:55:55 +0300

    solve loops lab

---
+++ lab3.py
@@ -0,0 +1,7 @@
+answer = input("what is the product of 7 * 24 ? ")
+
+while answer != "168":
+    print("Your Answer is wrong try again..")
+    answer = input("what is the product of 7 * 24 ? ")
+
+print("You answered this Question correctly")
